ab33a570df40938c7f0cc0f16cd397cdd1aec98f
marciogabrielsf <marciogdsf@gmail.com>
2023-03-04 02:09:36 -0300

    Main section style completed.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,6 +22,8 @@ export default function Header() {
                             </li>
                         )
                     })}
+
+                    <button className='hireMeButton'>Me Contrate Hoje!</button>
                 </ul>
 
             </nav>
