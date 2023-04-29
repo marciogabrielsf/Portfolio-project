cfc258a9f9174845ea5d1a5db79a7b99645dc0d8
marciogabrielsf <marciogdsf@gmail.com>
2023-04-29 19:53:54 -0300

    EmailJS now works at the form

--- src/pages/Contact/ContactForm/index.tsx
+++ src/pages/Contact/ContactForm/index.tsx
@@ -1,34 +1,94 @@
 
+import { FormEvent, useState } from 'react'
 import './styles.scss'
+import emailjs from '@emailjs/browser'
+
 
 export default function ContactForm() {
+
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState('')
+  const [isSuccess, setIsSuccess] = useState(false)
+
+  function handleSubmitButton(event: FormEvent) {
+    event.preventDefault()
+
+
+    if (email === '' || name === '' || message === '') {
+      return setError('Preencha todos os campos!')
+    }
+
+
+    const templateParams = {
+      from_name: name,
+      message,
+      email,
+    }
+
+
+    emailjs.send('service_14unkhh', 'template_etjbwmg', templateParams, '2QPJ_gxyll3ZRwKG6')
+      .then(() => {
+        setIsSuccess(true)
+        setError('')
+        setEmail('')
+        setName('')
+        setMessage('')
+      }
+      )
+      .catch((error) => setError(error))
+  }
+
+
   return (
     <div className="contact_form">
       <div className="shinyContainer contact_container">
         <h1 className='contactHeadline'>CONTATO</h1>
-        <form className='inputForm'>
+        <form className={`inputForm ${error && 'submit_error'}`}>
           <div className="contact_name">
-            <input className='inputBox' required={true} type="text" name="name" id="name" />
+            <input
+              className={`inputBox ${error && 'submit_error'}`}
+              required type="text"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              name="name"
+              id="name" />
             <label htmlFor="name">Nome</label>
           </div>
           <div className="contact_email">
-            <input className='inputBox' required={true} type="text" name="email" id="email" />
+            <input
+              className={`inputBox ${error && 'submit_error'}`}
+              required
+              type="text"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              name="email"
+              id="email" />
             <label htmlFor="email">E-mail</label>
           </div>
           <div className="contact_message">
             <textarea
-              className='inputBox'
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+              className={`inputBox ${error && 'submit_error'}`}
               id='message'
-              required={true}
+              required
               name='message'
-              rows={6}
-            ></textarea>
+              rows={6} />
             <label htmlFor="message">Mensagem</label>
           </div>
           <div className="contact_submit">
-            <button className='defaultButton submit_button' type="submit">
-              <span className='fa-regular fa-envelope'></span>Enviar
+            <button
+              className={`defaultButton submit_button ${(isSuccess && !error) && 'success'}`}
+              type="submit"
+              onClick={handleSubmitButton}>
+              <span
+                className={`${(isSuccess && !error) ? 'fa-solid fa-circle-check' : 'fa-regular fa-envelope'}`} />
+
+              {(isSuccess && !error) ? 'Enviado' : 'Enviar'}
             </button>
+            <p className={`${error ? 'submit_error' : 'error_hidden'}`}>{error}</p>
           </div>
         </form>
       </div>
