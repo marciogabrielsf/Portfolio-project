7ff243b107abe1647e5ad392ff0f0b750d1aab42
marciogabrielsf <marciogdsf@gmail.com>
2023-11-30 09:35:30 -0300

    removed duplicated experience

--- src/constants/experiences/index.ts
+++ src/constants/experiences/index.ts
@@ -6,7 +6,7 @@ export const experiences = [
 		period: "2022 - Atualmente",
 		description: [
 			"Gerenciamento de uma equipe pequena da agência de inovação.",
-			"Desenvolvimento de um sistema de marketplace para auxiliar as empresas juniores da universidade.",
+			"Desenvolvimento de um app de marketplace para auxiliar as empresas juniores da universidade.",
 		],
 	},
 	{
@@ -14,22 +14,13 @@ export const experiences = [
 		title: "Desenvolvedor Front-end Mobile",
 		company: "UserPay",
 		description: [
-			"Manutenção, restauração e desenvolvimento de features para o aplicativo principal da empresa, que é uma conta digital.",
-			"Resolução de problemas utilizando criptografia em React Native e Typescript.",
+			"Manutenção, restauração e desenvolvimento de features para a plataforma digital.",
+			"Resolução de problemas de criptografia em React Native e Typescript.",
 		],
 		period: "Julho de 2023 - Setembro de 2023",
 	},
 	{
 		id: 3,
-		title: "Desenvolvedor Fullstack",
-		company: "Freelancer",
-		description: [
-			"Desenvolvimento de Landing Pages, Sites e Aplicações Web para diversas empresas brasileiras.",
-		],
-		period: "2020 - Atualmente",
-	},
-	{
-		id: 4,
 		title: "Desenvolvedor Fullstack",
 		company: "Freelancer",
 		description: [
